c37890998a37cd10133ec59fc769d57c51df4f2b
seunghoon4176 <80800473+seunghoon4176@users.noreply.github.com>
2022-08-17 16:25:21 +0900

    Update df.py

--- df.py
+++ df.py
@@ -173,6 +173,35 @@ def lunchMenu():
 
     sendms.kakao_sendtext(lunch_menu)
 
+def TimeTable():
+    url = 'https://jeil.jje.hs.kr/jeil-h/0208/board/16996'
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        content = soup.select_one('#all-scroll > div > div > div.board-text > table.wb > tbody > tr:nth-child(1) > td.link > a')
+
+    a = str(content)
+    b = a.split(',')
+    c = b[1].strip("'")
+
+# 셀레니움
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    browser = webdriver.Chrome(options=options)
+
+    url_path = 'https://jeil.jje.hs.kr/jeil-h/0208/board/16996/{}'.format(c) # URL 저장 
+
+    browser.implicitly_wait(1) # 대기 시간
+    browser.get(url_path) # url로 이동
+    browser.implicitly_wait(1) # wait time
+
+    browser.find_element(By.XPATH,'//*[@id="all-scroll"]/div/form/div/article/div[3]/dl/dd/a[2]/img').click() #xpath값 클릭하기
+    time.sleep(3) # 대기 시간
+
+    
 def main():
 
     # sched = BackgroundScheduler()
