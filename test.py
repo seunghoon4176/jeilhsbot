b92bc87ba8310f6847134c7ea1c0bd769db67b87
seunghoon4176 <80800473+seunghoon4176@users.noreply.github.com>
2022-08-19 13:42:54 +0900

    yeah

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
+from calendar import month
+from itertools import count
 import os
+from re import L
+from time import time
 from bs4 import BeautifulSoup
 import requests
+from datetime import datetime
 
 url = 'https://jeil.jje.hs.kr/' # 학교 메인 홈페이지 URL
 
@@ -21,9 +26,37 @@ lunch_menu = "\n".join(result)
 
 import pandas as pd
 
-today ="8월19일(금)-탑재용.xlsx"
-filename = ("C:/Users/Jeju/Downloads/8월19일(금)-탑재용.xlsx")
+
+weekk = { 
+    "Sunday" : "일",
+    "Monday" : "월",
+    "Tuesday" : "화",
+    "Wednesday" : "수",
+    "Thursday" : "목",
+    "Friday" : "금",
+    "Saturday" : "토"
+ }
+tooday = datetime.today()
+monthh = tooday.month
+weekday = datetime.today().strftime('%A')
+today = datetime.today().strftime(f'{monthh}월%d일({weekk[weekday]})')
+
+filename = (f"C:/Users/ssh41/Downloads/{ today }-탑재용.xlsx")
 df=pd.read_excel(filename, engine = "openpyxl",usecols = "X")
+list_col2 = df.loc[:, df.columns[0]].to_list()
+print_list = []
+timeTable = []
 
+for i in list_col2 :
+    if type(i) != float :
+        print_list.append(i[0:i.find("\n")])
+    else :
+        continue
 
-print(df)
+for i in print_list :
+    if i == "" :
+        continue
+    timeTable.append(i)
+
+s = "\n".join(timeTable)
+print(s)
